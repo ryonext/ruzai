70f7ba834408ff9e937f89c0bb94d8d21222312b
ryonext <ryonext.s@gmail.com>
2015-07-15 20:39:09 +0900

    Implement suspending target.

--- lib/user_ban.rb
+++ lib/user_ban.rb
@@ -1,10 +1,31 @@
 require "user_ban/version"
+require "active_support"
+require "active_support/core_ext"
 
 module UserBan
   attr_accessor :suspention_expired_at
+  include ActiveSupport::Configurable
+  config_accessor :suspention_duration, :respawn_limit
+
+  self.configure do |config|
+    config.suspention_duration = 2.weeks
+    config.respawn_limit = 5
+  end
 
   def suspended?
     return false unless suspention_expired_at
     self.suspention_expired_at > Time.now
+  end
+
+  def suspended_until
+    return nil unless suspention_expired_at
+    remains = (self.suspention_expired_at - Time.now).to_i
+
+    # If time remains between 1 second ~ 23 hours 59 seconds, it displays '1 day'
+    (( remains / 1.day ) + 1 ).day
+  end
+
+  def suspend!
+    self.suspention_expired_at = UserBan.suspention_duration.from_now
   end
 end
--- spec/user_ban_spec.rb
+++ spec/user_ban_spec.rb
@@ -9,8 +9,9 @@ describe UserBan do
     expect(UserBan::VERSION).not_to be nil
   end
 
+  let(:user){ TestUser.new }
+
   describe 'suspended?' do
-    let(:user){ TestUser.new }
     subject { user.suspended? }
 
     context 'Non suspended user' do
@@ -34,6 +35,43 @@ describe UserBan do
 
       after do
         Timecop.return
+      end
+    end
+  end
+
+  describe 'suspended_until' do
+    subject { user.suspended_until }
+
+    context 'Non suspended user' do
+      it { is_expected.to be_nil }
+    end
+
+    context 'Suspended user' do
+      before do
+        user.suspention_expired_at = Time.now + 1
+      end
+      it { is_expected.to eq 1.day}
+    end
+  end
+
+  describe 'suspend!' do
+    subject { user.suspend! }
+
+    it "User's expired date is set to 2 weeks later." do
+      subject
+      expect(user.suspention_expired_at.to_date).to eq Date.today + 14.days
+    end
+
+    context 'Set expired date' do
+      before do
+        UserBan.configure do |config|
+          config.suspention_duration = 3.days
+        end
+      end
+
+      it "User's expired date is set to 3 days later." do
+        subject
+        expect(user.suspention_expired_at.to_date).to eq Date.today + 3.days
       end
     end
   end
