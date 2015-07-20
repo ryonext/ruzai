60350ede16700ccc7ee1930961f1e7cc03c64f58
ryonext <ryonext.s@gmail.com>
2015-07-21 00:53:13 +0900

    temp

---
+++ lib/generators/ruzai/templates/ruzai_parameters.rb
@@ -0,0 +1,6 @@
+class RuzaiParameters < ActiveRecord::Migration
+  def change
+    add_column :<%= model_class_name.tableize %>, :suspended_expired_at, :datetime, default: nil
+    add_column :<%= model_class_name.tableize %>, :suspended_count, :integer, default: 0
+  end
+end
