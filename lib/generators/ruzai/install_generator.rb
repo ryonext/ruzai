c5775490d6d33fdf6c1fc4b96f6902a40efbd8ae
ryonext <ryonext.s@gmail.com>
2015-07-26 23:39:20 +0900

    Set model name by args

--- lib/generators/ruzai/install_generator.rb
+++ lib/generators/ruzai/install_generator.rb
@@ -3,7 +3,8 @@ module Ruzai
     source_root File.expand_path('../templates', __FILE__)
 
     def install
-      copy_file "ruzai_parameters.rb", "#{Time.now.strftime("%Y%m%d%H%M%d")}_db/migrate/ruzai_parameters.rb"
+      @model_class_name = args[0] || "User"
+      template "ruzai_parameters.rb", "db/migrate/#{Time.now.strftime("%Y%m%d%H%M%d")}_ruzai_parameters.rb"
     end
   end
 end
--- lib/generators/ruzai/templates/ruzai_parameters.rb
+++ lib/generators/ruzai/templates/ruzai_parameters.rb
@@ -1,6 +1,6 @@
 class RuzaiParameters < ActiveRecord::Migration
   def change
-    add_column :<%= model_class_name.tableize %>, :suspended_expired_at, :datetime, default: nil
-    add_column :<%= model_class_name.tableize %>, :suspended_count, :integer, default: 0
+    add_column :<%= @model_class_name.tableize %>, :suspended_expired_at, :datetime, default: nil
+    add_column :<%= @model_class_name.tableize %>, :suspended_count, :integer, default: 0
   end
 end
