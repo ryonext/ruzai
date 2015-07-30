74dc74e60d1f474742411dc140eda823fbcd2d4a
ryonext <ryonext.s@gmail.com>
2015-07-30 17:22:41 +0900

    Use Bundler.require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
-Bundler.require
-require 'timecop'
+Bundler.require(:default, :development)
+
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do
